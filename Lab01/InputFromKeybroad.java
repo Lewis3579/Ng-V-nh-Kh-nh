9d6b09a713d8503ec740ef5d34cbf8b4f966a456
Lewis3579 <117494220+Lewis3579@users.noreply.github.com>
2023-04-14 07:53:28 +0700

    Update InputFromKeybroad.java

--- Lab01/InputFromKeybroad.java
+++ Lab01/InputFromKeybroad.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class InputFromKeybroad {
+    public static void main(String[] args) {
+        Scanner keybroad = new Scanner(System.in);
+        System.out.println("What your name?");
+        String strName = keybroad.nextLine();
+        System.out.println("How old are you?");
+        int iAge = keybroad.nextInt();
+        keybroad.nextLine();
+        System.out.println("How tall are you?");
+        double dHeight = keybroad.nextDouble();
+
+        System.out.println("Mrs/Ms. " + strName + ", " + iAge + " years old. " + " Your height is " + dHeight + ".");
+    }
+}
