aae5c02d4e64636e06e1c9397f873231f99dba57
Lewis3579 <117494220+Lewis3579@users.noreply.github.com>
2023-04-16 22:46:35 +0700

    Update Equation.java

--- Lab01/Equation.java
+++ Lab01/Equation.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
 import javax.swing.JOptionPane;
 
 public class Equation {
@@ -18,13 +20,16 @@ public class Equation {
                 case 1:
                     linear1();
                     break;
+                case 2:
+                    linear2();
+                    break;
             }
         } while (menu != 0);
         System.exit(0);
     }
 
     static void linear1() {
-        Scanner keybroad = new Scanner(System.in);
+
         String Sa = JOptionPane.showInputDialog(null, "a:", "Linear1", 3);
         double a = Double.parseDouble(Sa);
         String Sb = JOptionPane.showInputDialog(null, "b:", "Linear1", 3);
@@ -32,4 +37,36 @@ public class Equation {
         double x = -(b / a);
         JOptionPane.showMessageDialog(null, String.format("The result is: %.2f", x), "The result", 1);
     }
+
+    static void linear2() {
+
+        String Sa1 = JOptionPane.showInputDialog(null, "a1:", "Linear2", 3);
+        double a1 = Double.parseDouble(Sa1);
+        String Sb1 = JOptionPane.showInputDialog(null, "b1:", "Linear2", 3);
+        double b1 = Double.parseDouble(Sb1);
+        String Sc1 = JOptionPane.showInputDialog(null, "c1:", "Linear2", 3);
+        double c1 = Double.parseDouble(Sc1);
+        String Sa2 = JOptionPane.showInputDialog(null, "a2:", "Linear2", 3);
+        double a2 = Double.parseDouble(Sa2);
+        String Sb2 = JOptionPane.showInputDialog(null, "b2:", "Linear2", 3);
+        double b2 = Double.parseDouble(Sb2);
+        String Sc2 = JOptionPane.showInputDialog(null, "c1:", "Linear2", 3);
+        double c2 = Double.parseDouble(Sc1);
+        double D = a1 * b2 - a2 * b1;
+        double Dx1 = c1 * b2 - c2 * b1;
+        double Dx2 = a1 * c2 - a1 * c2;
+        double x1 = 0;
+        double x2 = 0;
+        if (D == 0 && Dx1 == 0 && Dx2 == 0) {
+            JOptionPane.showMessageDialog(null, "This equation have infinity solutions!", "The result", 1);
+        } else if (D == 0 && (Dx1 != 0 || Dx2 != 0)) {
+            JOptionPane.showMessageDialog(null, "This equation have no solutions!", "The result", 1);
+        } else {
+            x1 = Dx1 / D;
+            x2 = Dx2 / D;
+        }
+        JOptionPane.showMessageDialog(null, String.format("The result is: %.2f %2f", x1, x2), "The result", 1);
+    }
+
 }
+
