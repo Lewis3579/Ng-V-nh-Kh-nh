9f5c40f3c7f4b1ec64e4ee2ccdfa0ffbb1352bb0
Lewis3579 <117494220+Lewis3579@users.noreply.github.com>
2023-04-28 17:40:28 +0700

    Add files via upload

---
+++ Lab02/DigitalVideoDics.java
@@ -0,0 +1,74 @@
+
+public class DigitalVideoDics {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDirector() {
+        return this.director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return this.cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public DigitalVideoDics(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDics(String category, String title, Float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDics(String director, String category, String title, Float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+    }
+
+    public DigitalVideoDics(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+        this.length = length;
+    }
+
+}
