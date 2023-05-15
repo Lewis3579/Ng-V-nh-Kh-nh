0aaa1f154428e5922467ea0df895bd0f55455caf
Lewis3579 <117494220+Lewis3579@users.noreply.github.com>
2023-05-15 21:23:21 +0700

    Update Cart.java

--- Lab02/Cart.java
+++ Lab02/Cart.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Cart {
     public static final int MAX_NUMBER_ORDERED = 20;
     private DigitalVideoDics[] itemsOrdered = new DigitalVideoDics[MAX_NUMBER_ORDERED];
@@ -10,6 +11,27 @@ public class Cart {
         else {
             itemsOrdered[qtyOrdered] = item;
             this.qtyOrdered++;
+        }
+    }
+    
+    public void addDigitalVideoDics(DigitalVideoDics item1, DigitalVideoDics item2) {
+        if (qtyOrdered + 2 >= MAX_NUMBER_ORDERED - 1)
+            System.out.println("The cart is full.");
+        else {
+            itemsOrdered[qtyOrdered] = item1;
+            itemsOrdered[qtyOrdered + 1] = item2;
+            this.qtyOrdered = this.qtyOrdered + 2;
+        }
+    }
+    
+        public void addDigitalVideoDics(ArrayList<DigitalVideoDics> dvdList) {
+        if (qtyOrdered + dvdList.size() > MAX_NUMBER_ORDERED)
+            System.out.println("The cart will be full");
+        else {
+            for (int i = 0; i < dvdList.size(); i++) {
+                itemsOrdered[qtyOrdered] = dvdList.get(i);
+                this.qtyOrdered++;
+            }
         }
     }
 
