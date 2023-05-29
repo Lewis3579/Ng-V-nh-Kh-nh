e79d0354b2628025c129898db8fde9bb7804355a
Lewis3579 <117494220+Lewis3579@users.noreply.github.com>
2023-05-29 21:31:03 +0700

    Add files via upload

---
+++ OOB lab/AimsProject/src/Store.java
@@ -0,0 +1,30 @@
+import Media.Media;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    public static final int MAX_NUMBER_ORDERED = 1000;
+    private static ArrayList<Media> itemsInStore = new ArrayList<Media>();
+    public static int j;
+
+    public static void addMedia(Media item) {
+        if (itemsInStore.size() == MAX_NUMBER_ORDERED)
+            System.out.println("The cart is full.");
+        else {
+            itemsInStore.add(item);
+        }
+    }
+
+    public static void removeMedia(Media item) {
+        if (itemsInStore.size() == 0)
+            System.out.println("There is no item!");
+        else {
+            for (int i = 0; i < itemsInStore.size(); i++) {
+                if (itemsInStore.get(i) == item) {
+                    itemsInStore.remove(i);
+                    j = i;
+                }
+            }
+        }
+    }
+}
